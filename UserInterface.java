9277ce144b73df3e351e96f40ad859ba197aa95a
franciscofabio <franciscofabio18@hotmail.com>
2015-11-09 18:12:56 -0200

    Update Scanner
    Classe scanner

--- UserInterface.java
+++ UserInterface.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class UserInterface implements ActionListener{
     JFrame frame;
